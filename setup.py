8a5ae3badeacf77530f9a920d569b8adb6d3ac3c
Moumita <paulmoumita192@gmail.com>
2026-03-15 22:34:08 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Moumita Paul",
+    author_email="paulmoumita192@gmail.com",
+    packages=find_packages()
+)
